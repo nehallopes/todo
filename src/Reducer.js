0ad2518ebdbf52e015de683326f69212a1e59dc1
Nehal lopes <nehallopes@gmail.com>
2023-10-24 17:08:29 -0500

    commit

---
+++ src/Reducer.js
@@ -0,0 +1,45 @@
+export const userReducer = (state, action) => {
+    switch (action.type) {
+      case "LOGIN":
+      case "REGISTER":
+        return action.username;
+      case "LOGOUT":
+        return "";
+      default:
+        return state;
+    }
+  }
+
+  export const todoReducer = (state, action) => {
+
+    switch (action.type) {
+      case "CREATE_TODO":
+
+        const newTodo = {
+          title: action.title,
+          content: action.content,
+          author: action.author,
+          dispatch: action.dispatch,
+        };
+        return [newTodo, ...state];
+      case "TOGGLE_TODO":
+        if(!action.complete){
+          document.getElementById(action.title).innerHTML="Completed on: "+new Date(Date.now()).toString();
+        }
+        else{
+          document.getElementById(action.title).innerHTML="";
+        }
+        return state;
+      case "DELETE_TODO":
+
+        const index = state.map(t => t.title).indexOf(action.title);
+        const stateTemp = [
+          ...state.slice(0, index),
+          ...state.slice(index + 1)
+        ];
+        return stateTemp;
+
+      default:
+        return state;
+    }
+  }
